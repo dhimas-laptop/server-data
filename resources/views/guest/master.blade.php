a72ec999db740c3e4e63c5b2635ece8c8fbefd88
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2022-07-06 22:15:07 +0700

    update bug

--- resources/views/guest/master.blade.php
+++ resources/views/guest/master.blade.php
@@ -67,7 +67,7 @@
                             </div>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarWelcome" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarWelcome" role="button" data-toggle="dropdown" aria-haspopup="false" aria-expanded="false">
                                 Pelayanan
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarWelcome">

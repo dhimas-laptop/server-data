ddb018519e000db08c875479470c7e11b5602e69
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2022-11-15 16:50:13 +0700

    update API 15-11-22

--- config/app.php
+++ config/app.php
@@ -178,7 +178,6 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,
     ],
 
     /*
@@ -195,7 +194,6 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-        'PDF' => Barryvdh\DomPDF\Facade::class,
     ])->toArray(),
 
 ];

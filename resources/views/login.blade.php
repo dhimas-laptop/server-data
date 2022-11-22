9923c22f19013509065a26f2993285c836fbb85d
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2022-11-22 14:49:27 +0700

    perbaharui nama instansi

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,7 +18,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="#"><b>SiPuda</b>BPDASHL</a>
+    <a href="#"><b>SiPuda</b>BPDAS</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">

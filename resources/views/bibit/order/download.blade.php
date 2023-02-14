c5c8d551aa283401200c5cba1940f231befa6bc4
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2023-02-14 14:08:30 +0700

    map trial 2

--- resources/views/bibit/order/download.blade.php
+++ resources/views/bibit/order/download.blade.php
@@ -2,6 +2,8 @@
   <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css" integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
   <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js" integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+  {{ $lt = $data['data']['order'][0]['latitude'] }}
+  {{ $lg = $data['data']['order'][0]['longitude'] }}
   <script>
     var lt = {{ $lt }};
     var lg = {{ $lg }};

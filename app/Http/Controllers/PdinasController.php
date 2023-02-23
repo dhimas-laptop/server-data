ad6ed0cb1fc1f9879b8cae57ba06154105bb2dfe
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2023-02-23 13:53:33 +0700

    update setyo rini

--- app/Http/Controllers/PdinasController.php
+++ app/Http/Controllers/PdinasController.php
@@ -41,7 +41,7 @@ class PdinasController extends Controller
                 ->where('nomor_spt','LIKE','%'.$role.'%')
                 ->get();
         } 
-        if (auth::user()->role === 'admin') {
+        if (auth::user()->role === 'admin' || auth::user()->id === 16) {
             $spd = spd::where('tgl_spt' , $today)->get();
         }
         

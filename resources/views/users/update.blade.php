9c8dd0507df841c99fa1b909ee6d21f6ba65495f
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2023-02-28 16:24:35 +0700

    update user

--- resources/views/users/update.blade.php
+++ resources/views/users/update.blade.php
@@ -47,7 +47,7 @@
           <div class="form-group row">
             <label for="inputEmail3" class="col-sm-2 col-form-label">Password</label>
             <div class="col-sm-10">
-              <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Isi Password Baru" name="password" >
+              <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Isi Password Baru" name="password" value="">
             </div>
           </div>  
           <div class="form-group row">

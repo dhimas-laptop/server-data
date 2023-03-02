229b5ea0ad435e918fa73764c0040c4407a6f8b8
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2023-03-02 12:52:00 +0700

    update bug

--- resources/views/pdinas.blade.php
+++ resources/views/pdinas.blade.php
@@ -400,6 +400,9 @@
 <script type="text/javascript">
 $(document).ready(function() {
     var table = $('#example').DataTable( {
+        rowReorder: {
+            selector: 'td:nth-child(2)'
+        },
         responsive: true
     } );
 } );

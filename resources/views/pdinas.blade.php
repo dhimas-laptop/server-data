9ae6bc8605d9d4b452cfd729ed5968209e757b22
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2023-03-02 12:32:15 +0700

    pdinas view update

--- resources/views/pdinas.blade.php
+++ resources/views/pdinas.blade.php
@@ -400,9 +400,6 @@
 <script type="text/javascript">
 $(document).ready(function() {
     var table = $('#example').DataTable( {
-        rowReorder: {
-            selector: 'td:nth-child(2)'
-        },
         responsive: true
     } );
 } );

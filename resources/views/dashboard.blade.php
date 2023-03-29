6ceae01b5039bac06b4babcf9419c0c62d0cf469
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2023-03-29 12:29:39 +0700

    detail dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,31 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Dashborad</h1>
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Update Patch 29 Maret 2023</h3>
+                        <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i></button>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+                    Pembaharuan:<br>
+                                        
+                    <strong> 1. Perjalanan Dinas = <br></strong>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Pembukaan blokir 524114<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Penyesuaian penambahan 524114<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Perbaikan download 524114<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- perbaikan format 524114<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Finalisasi modul 524114 pada perjalanan dinas<br>
+
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
@@ -22,17 +46,20 @@
 
                     <div class="card-body">
                     Pembaharuan:<br>
-                    <strong> 1. Perjalanan Dinas = <br></strong>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Pembukaan blokir 524114<br>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Penyesuaian penambahan 524114<br>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Perbaikan download 524114<br>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- perbaikan format 524114<br>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Finalisasi modul 524114 pada perjalanan dinas<br>
+                    <strong>1. Dashboard =<br></strong>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Pemberitahuan akan muncul di dashboard<br><br>
+                    <strong> 2. Perjalanan Dinas = <br></strong>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Tampilan per tahun,perbulan, dan pertanggal di hapus<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Sekarang tampilan di bagi per akun 524111, 524114, dan 524119(blokir)<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Input data disesuaikan akun perjalanan dinas<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Tombol hapus disediakan untuk semua user<br>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;- Tampilan Perseksi dan atau pernama user yang aktif <br>
                     <br><br>
 
                     </div>
                 </div>
             </div>
+        </div>
     </div>
 </section>
 @endsection

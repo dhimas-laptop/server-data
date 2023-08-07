85366335f0d06dd4b77521594e5f10e7c20d206f
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2023-08-07 10:59:52 +0700

    update

--- app/Http/Controllers/AbsensiController.php
+++ app/Http/Controllers/AbsensiController.php
@@ -130,7 +130,7 @@ class AbsensiController extends Controller
 
             foreach ($request->file('gambar') as $gambar) {
             $nama_gambar = time() . '-' . $gambar->getClientOriginalName();
-            $gambar->move(public_path('gambarpl'), $nama_gambar);
+            $gambar->move(base_path('public/gambarpl'), $nama_gambar);
 
             gambarlaporan::insert([
                 'gambar' => $nama_gambar,

1449ec21573737cfe19831a93953fc2c435fe61a
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2023-08-07 08:48:10 +0700

    update nama

--- resources/views/absensi/index.blade.php
+++ resources/views/absensi/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Bibit Gratis</title>
+    <title>Absensi PL BPDAS SJD</title>
 
     <link rel="stylesheet" href="https://bibit.bpdas-sjd.id/vendor/adminlte/css/adminlte.min.css">
     <link href="https://bibit.bpdas-sjd.id/vendor/fontawesome/css/all.min.css" rel="stylesheet">

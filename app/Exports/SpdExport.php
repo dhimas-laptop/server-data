23e1367bf42fae1521ee2df3a9b586fcc7cc1d2f
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2023-12-07 14:06:51 +0700

    Revert "update"
    This reverts commit d3d2e7244d8305c16681019b7d729a258d4e8f9b.

--- app/Exports/SpdExport.php
+++ app/Exports/SpdExport.php
@@ -8,13 +8,49 @@ use Maatwebsite\Excel\Concerns\Exportable;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Events\BeforeSheet;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class SpdExport implements FromView, ShouldAutoSize
+class SpdExport implements FromView, ShouldAutoSize, WithEvents
 {
     use Exportable;
+    public function forYear($no)
+    {
+        $this->no = $no;
+        return $this;
+    }
+    
+    public function forRole($role)
+    {
+        $this->role = $role;
+        return $this;
+    }
+    
+    public function registerEvents(): array
+    {
+        return [
+            BeforeSheet::class => function (BeforeSheet $event) {
+                $event->sheet->getPageSetup()->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
+                $event->sheet->getPageSetup()->setPaperSize(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::PAPERSIZE_A4);
+                $event->sheet->getPageSetup()->setFitToWidth(1);
+                $event->sheet->getPageSetup()->setFitToHeight(0);
+            },
+            
+        ];
+    }
+
     public function view(): View
     {
+        if ($this->role === 'admin') {
             $query = spd::get();
+        } else {
+            $query = spd::where('nomor_spt', $this->no)->orderBy('tgl_spt', 'ASC')->get();
+        }
         
         return view('report.spd', [
             'spd' => $query , 'no' => 1 

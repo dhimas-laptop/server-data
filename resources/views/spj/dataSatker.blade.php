39318af83f182fab8d6d4006d3a67504f991698b
dhimas-laptop <104683220+dhimas-laptop@users.noreply.github.com>
2025-08-12 12:18:33 +0700

    update

--- resources/views/spj/dataSatker.blade.php
+++ resources/views/spj/dataSatker.blade.php
@@ -15,7 +15,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Penandatangan</h1>
+            <h1>Data Satker</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
